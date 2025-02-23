b6377091eb61ae8e28924cc8d2493f4eb9492c25
EsleySaab <esleysantana.sa@gmail.com>
2025-02-23 10:07:51 -0300

    feat: add initial finish order dialog component

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,43 +1,56 @@
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
-import { useContext } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useContext, useState } from "react";
 import { CartContext } from "../context/cart";
 import CartItem from "./cart-product-item";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { formatCurrency } from "@/helpers/format-currency";
+import FinishOrderDialog from "./finish-order-dialog";
 
 const CartSheet = () => {
-  const {isOpen, toggleCart, products, total} = useContext(CartContext)
+  const [finishOrderDialogIsOpen, setFinishOrderDialogIsOpen] = useState(false);
+  const { isOpen, toggleCart, products, total } = useContext(CartContext);
 
-  return ( 
+  return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-  <SheetContent className="w-[80%]">
-    <SheetHeader>
-      <SheetTitle className="text-left">Sacola</SheetTitle>
-      
-    </SheetHeader>
-    <div className="py-5 flex h-full flex-col">
-      <div className="flex-auto">
-        {products.map((product) => (
-      <CartItem product={product} key={product.id}/>
-    ))}
-      </div>
-
-      <Card className="mb-6">
-        <CardContent className="p-5">
-          <div className="flex justify-between">
-            <p className="text-sm text-muted-foreground">Total</p>
-            <p className="font-semibold text-sm">{formatCurrency(total)}</p>
+      <SheetContent className="w-[80%]">
+        <SheetHeader>
+          <SheetTitle className="text-left">Sacola</SheetTitle>
+        </SheetHeader>
+        <div className="flex h-full flex-col py-5">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CartItem product={product} key={product.id} />
+            ))}
           </div>
-        </CardContent>
-      </Card>
-    <Button className="w-full rounded-full">
-      Finalizar pedido
-    </Button>
-    </div>
-  </SheetContent>
-</Sheet>
-   );
-}
- 
+
+          <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{formatCurrency(total)}</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Button
+            onClick={() => setFinishOrderDialogIsOpen(true)}
+            className="w-full rounded-full"
+          >
+            Finalizar pedido
+          </Button>
+          <FinishOrderDialog
+            open={finishOrderDialogIsOpen}
+            onOpenChange={setFinishOrderDialogIsOpen}
+          />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
 export default CartSheet;
