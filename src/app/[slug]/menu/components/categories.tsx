370dfb40fcc6ece9c4c14a35c08d427906aa0642
EsleySaab <esleysantana.sa@gmail.com>
2025-02-24 16:39:39 -0300

    feat: ensure cart summary is shown on restaurant page

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -5,8 +5,11 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import {  Prisma, Restaurant } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Products from "./products";
+import { CartContext } from "../context/cart";
+import { formatCurrency } from "@/helpers/format-currency";
+import CartSheet from "./cart-sheet";
 
 interface RestaurantCategoriesProps {
   restaurant: Prisma.RestaurantGetPayload<{
@@ -27,6 +30,8 @@ type MenuCategoriesWithProducts = Prisma.MenuCategoryGetPayload<{
 }>
 
 const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
+  const {total, products, toggleCart, totalQuantity} = useContext(CartContext)
+
   const [selectedCategory, setSelectedCategory] = useState<MenuCategoriesWithProducts>(restaurant.menuCategories[0])
 
   const handleCategoryClick = (category: MenuCategoriesWithProducts) => {
@@ -74,6 +79,22 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
         <h3 className="font-semibold px-5 pt-2">{selectedCategory.name}</h3>
 
       <Products products={selectedCategory.products}/>
+      {products.length > 0 && (
+        <div className="fixed- bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
+            <div>
+              <p className="text-xs text-muted-foreground">Total dos pedidos</p>
+              <p className="text-sm font-semibold">
+                {formatCurrency(total)}
+                <span className="text-xs font-normal text-muted-foreground">/ 
+                   {totalQuantity } {totalQuantity > 1 ? "itens" : "item"}</span>
+              </p>
+            </div>
+            <Button onClick={toggleCart}>
+              Ver sacola
+            </Button>
+            <CartSheet />
+        </div>
+      )}
     </div>
   );
 };
--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -12,6 +12,7 @@ export interface ICartContext {
   isOpen: boolean
   products: CartProduct[]
   total: number
+  totalQuantity: number
   toggleCart: () => void
   addProduct: (product: CartProduct) => void
   decreaseProductQuantity: (productId: string) => void
@@ -22,12 +23,13 @@ export interface ICartContext {
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   products: [],
+  total: 0,
+  totalQuantity: 0,
   toggleCart: () => {},
   addProduct: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
   removeProduct: () => {},
-  total: 0
 })
 
 export const CartProvider = ({children}:{children: ReactNode}) => {
@@ -36,6 +38,10 @@ export const CartProvider = ({children}:{children: ReactNode}) => {
 
   const total = products.reduce((acc, product) => {
     return acc + product.price * product.quantity
+  }, 0)
+
+  const totalQuantity = products.reduce((acc, product) => {
+    return acc + product.quantity
   }, 0)
 
   const toggleCart = () => {
@@ -98,6 +104,7 @@ export const CartProvider = ({children}:{children: ReactNode}) => {
       isOpen,
       products,
       total,
+      totalQuantity,
       toggleCart,
       addProduct,
       decreaseProductQuantity,
